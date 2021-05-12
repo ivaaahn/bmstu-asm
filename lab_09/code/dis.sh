7cfa5f01dcae11898298d5be0008ee0a1a701981
d_ivahnencko <ivahnencko01@gmail.com>
2021-05-12 09:50:09 +0300

    FIX: script for disassembly

--- lab_09/code/dis.sh
+++ lab_09/code/dis.sh
@@ -6,15 +6,15 @@ then
 fi
 
 
-gcc -DASM -I./includes/ -masm=intel -O0 -S -m80387 -mfpmath=387 sources/measures64.c -o ./dis/32b_x87.s -lm
-gcc -DASM -I./includes/ -masm=intel -O0 -S -mno-80387 sources/measures64.c -o ./dis/32b_no_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -m80387 -mfpmath=387 sources/measures32.c -o ./dis/32b_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -mno-80387 sources/measures32.c -o ./dis/32b_no_x87.s -lm
 
 
-gcc -DASM -I./includes/ -masm=intel -O0 -S -m80387 -mfpmath=387 sources/measures64.c -o ./dis/64b_x87.s -lm
-gcc -DASM -I./includes/ -masm=intel -O0 -S -mno-80387 sources/measures64.c -o ./dis/64b_no_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -m80387 -mfpmath=387 sources/measures64.c -o ./dis/64b_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -mno-80387 sources/measures64.c -o ./dis/64b_no_x87.s -lm
 
 
-gcc -DASM -I./includes/ -masm=intel -O0 -S -mno-80387 sources/measures64.c -o ./dis/80b_no_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -m80387 -mfpmath=387 sources/measures80.c -o ./dis/80b_no_x87.s -lm
 
-gcc -DASM -I./includes/ -masm=intel -O0 -S -m80387 -mfpmath=387 sources/measures_sin.c -o ./dis/sin_x87.s -lm
-gcc -DASM -I./includes/ -masm=intel -O0 -S -mno-80387 sources/measures_sin.c -o ./dis/sin_no_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -m80387 -mfpmath=387 sources/measures_sin.c -o ./dis/sin_x87.s -lm
+gcc -DASM -I./includes/ -masm=intel -O0 -S -DX87 -mno-80387 sources/measures_sin.c -o ./dis/sin_no_x87.s -lm
